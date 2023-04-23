3fe2324a26092cd651f9e24aa8c64e795e261aff
peasoon <vasyarichman@mail.ru>
2023-04-23 09:21:50 +0300

    added get by category

--- controllers/ProductsController.js
+++ controllers/ProductsController.js
@@ -1,30 +1,50 @@
 import { pool } from "../app.js";
 
 export const getAllProducts = (request, response) => {
+  const category = request.query.category;
+  console.log(category);
+  if (!category) {
+    pool.query(
+      "SELECT * FROM products ORDER BY product_id ASC",
+      (error, results) => {
+        if (error) {
+          return response.status(500).json({ message: "Database error" });
+          //throw error
+        }
+        return response.status(200).json(results.rows);
+      }
+    );
+  } else {
+    pool.query(
+      "SELECT * FROM products WHERE category=$1 ORDER BY product_id ASC",
+      [category],
+      (error, results) => {
+        if (error) {
+          return response.status(500).json({ message: "database error" });
+          //throw error
+        } else if (results.rows.length === 0) {
+          return response.status(500).json({ message: "no such category" });
+        } else return response.status(200).json(results.rows);
+      }
+    );
+  }
+};
+
+export const getProductById = (request, response) => {
+  const slug = String(request.params.slug);
+
   pool.query(
-    "SELECT * FROM products ORDER BY product_id ASC",
+    "SELECT * FROM products WHERE slug = $1",
+    [slug],
     (error, results) => {
       if (error) {
-        response.status(500).json({ message: "Database error" });
+        return response.status(404).json({ message: "Product not found" });
         //throw error
+      }
+      if (results.rows.length === 0) {
+        return response.status(404).json({ message: "Product not found" });
       }
       response.status(200).json(results.rows);
     }
   );
 };
-
-
-export const getProductById = (request, response) => {
-  const slug = String(request.params.slug)
-
-  pool.query('SELECT * FROM products WHERE slug = $1', [slug], (error, results) => {
-    if (error) {
-      return response.status(404).json({ message: "Product not found" });
-      //throw error
-    }
-    if(results.rows.length===0) {
-      return response.status(404).json({ message: "Product not found" });
-    }
-    response.status(200).json(results.rows)
-  })
-}
